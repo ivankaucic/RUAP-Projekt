3be2364e9e4c07a59a0343e2f479cfa319546f5e
Ivan Kaucic <ivan.kaucic01@gmail.com>
2020-12-02 18:54:50 +0100

    Gotova aplikacija

---
+++ Aplikacija/Aplikacija.cpp
@@ -0,0 +1,12 @@
+// Aplikacija.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <stdio.h>
+
+int main()
+{
+    printf("Pozdrav iz aplikacije od Ivan Kaucic");
+    return 0;
+}
+
